64d7027e38dc94f98f76004a23c4eab2477378ae
ADESH MAHTO <141711491+AdeshStack@users.noreply.github.com>
2025-09-13 22:21:09 +0530

    Update Order.java

--- orderservice/orderservice/src/main/java/com/tiffinsystem/orderservice/entity/Order.java
+++ orderservice/orderservice/src/main/java/com/tiffinsystem/orderservice/entity/Order.java
@@ -18,8 +18,10 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "product_id")
     private Long productId;
 
     private  Integer quantity;
