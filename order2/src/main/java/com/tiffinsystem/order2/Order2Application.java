56d29e0dcca761879462abecc5f87f81f81a0082
ADESH MAHTO <141711491+AdeshStack@users.noreply.github.com>
2025-09-21 14:26:05 +0530

    Update Order2Application.java

--- order2/src/main/java/com/tiffinsystem/order2/Order2Application.java
+++ order2/src/main/java/com/tiffinsystem/order2/Order2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.tiffinsystem.order2.ExternalService.FC")
 public class Order2Application {
 
 	public static void main(String[] args) {
